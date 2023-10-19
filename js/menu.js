c2d6834a7199b4f930cb6ab2527ee7d2ecda630a
nagarcia94 <000nagarcia@gmail.com>
2023-10-19 16:51:33 -0300

    interatividade no menu com JS

--- js/menu.js
+++ js/menu.js
@@ -3,15 +3,25 @@
 
 // Estou selecionando o elemento (através de descendência) que acionará o menu, utilanzando o DOM (document) e queryselector para selecionar o que eu quero. 
 const botaoMenu = document.querySelector("nav h2")
-console.log(botaoMenu)
+/* console.log(botaoMenu) */
 
 
 // Selecionando a lista /menu através da classe
 const menu = document.querySelector(".menu");
-console.log(menu)
+/* console.log(menu) */
 
 // Selecionado o link que está dentro do nav h2 (foi usado o botaoMenu pois ja tinha utilizado ali em cima entao ja estava gravado na memoria o nav h2, como so faltava o link "a" entao usei o botaoMenu e depois selecionei o "a")
 const textoBotao = botaoMenu.querySelector("a");
-console.log(textoBotao)
+/* // console.log(textoBotao) - para ver se funcionou no console. */
+
+// Para efetuar a programaçao que eu mostrei a cima usando o evento.
+
+botaoMenu.addEventListener("click", function(event){
+    
+    /* Anular/prevenir o comportamento padrão do link */
+    event.preventDefault(); 
+    menu.classList.toggle("aberto");
+});
+
 
 
