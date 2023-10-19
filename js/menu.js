f6c4ab5cd0f29b038be890ee53803351d6f41470
nagarcia94 <000nagarcia@gmail.com>
2023-10-19 16:32:54 -0300

    JS no menu do simplicyt

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+"use strict"
+
+
+// Estou selecionando o elemento (através de descendência) que acionará o menu, utilanzando o DOM (document) e queryselector para selecionar o que eu quero. 
+const botaoMenu = document.querySelector("nav h2")
+console.log(botaoMenu)
+
+
+// Selecionando a lista /menu através da classe
+const menu = document.querySelector(".menu");
+console.log(menu)
+
+// Selecionado o link que está dentro do nav h2 (foi usado o botaoMenu pois ja tinha utilizado ali em cima entao ja estava gravado na memoria o nav h2, como so faltava o link "a" entao usei o botaoMenu e depois selecionei o "a")
+const textoBotao = botaoMenu.querySelector("a");
+console.log(textoBotao)
+
+
