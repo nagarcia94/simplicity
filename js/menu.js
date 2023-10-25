9dafb5775b218972120d1d95412aabf34e3a5192
nagarcia94 <000nagarcia@gmail.com>
2023-10-25 14:11:13 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,38 +1,24 @@
-"use strict"
+"use strict";
 
+// Selecionando o elemento (através de descendência) que acionará o menu
+const botaoMenu = document.querySelector("nav h2");
 
-// Estou selecionando o elemento (através de descendência) que acionará o menu, utilanzando o DOM (document) e queryselector para selecionar o que eu quero. 
-const botaoMenu = document.querySelector("nav h2")
-/* console.log(botaoMenu) */
-
-
-// Selecionando a lista /menu através da classe
+// Selecionando a lista/menu através da classe
 const menu = document.querySelector(".menu");
-/* console.log(menu) */
 
-// Selecionado o link que está dentro do nav h2 (foi usado o botaoMenu pois ja tinha utilizado ali em cima entao ja estava gravado na memoria o nav h2, como so faltava o link "a" entao usei o botaoMenu e depois selecionei o "a")
+// Selecionando o link que está dentro do nav h2
 const textoBotao = botaoMenu.querySelector("a");
-/* // console.log(textoBotao) - para ver se funcionou no console. */
-
-// Para efetuar a programaçao que eu mostrei a cima usando o evento.
 
 botaoMenu.addEventListener("click", function(event){
-    
-    /* Anular/prevenir o comportamento padrão do link */
-    event.preventDefault(); 
+    /* Anular/prevenir o comportamento do link */
+    event.preventDefault();
     menu.classList.toggle("aberto");
 
     if (menu.classList.contains("aberto")) {
-
         textoBotao.innerHTML = "Fechar &times;";
-
     } else {
-
         textoBotao.innerHTML = "Menu &equiv;";
-
     }
-
-
 });
 
 
