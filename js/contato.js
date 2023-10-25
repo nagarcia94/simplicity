1064e5836f12d1e15bdef1ff017e1bf699f548d4
nagarcia94 <000nagarcia@gmail.com>
2023-10-25 14:45:42 -0300

    Update contato.js

--- js/contato.js
+++ js/contato.js
@@ -72,3 +72,38 @@ botaoBuscar.addEventListener("click", async function(event){
         campoEstado.value = dados.uf;
     }
 });
+
+
+// Programação do Formspree 
+
+
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: formulario.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Seus dados foram enviados! Obrigado.";
+          formulario.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    formulario.addEventListener("submit", handleSubmit)
+
+
